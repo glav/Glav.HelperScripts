f10b3d8c0d3a59b63309ed295b4d26cc140d6956
Paul Glavich <glav@theglavs.com>
2016-11-10 17:17:54 -0800

    Add list-helpers to list out all helper scripts

---
+++ List-Helpers.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+echo List of Helper Scripts
+echo ~~~~~~~~~~~~~~~~~~~~~~
+echo.
+echo Docker Related
+echo - Docker-GetFirstContainerId
+echo - Docker-StopAllContainers
+echo - Docker-RemoveAllStoppedContainers
+echo - Docker-StartNginx
---
+++ List-Helpersx.ps1
@@ -0,0 +1,8 @@
+Write-Host  List of Helper Scripts
+Write-Host  ~~~~~~~~~~~~~~~~~~~~~~
+Write-Host  
+Write-Host  Docker Related
+Write-Host  > Docker-GetFirstContainerId
+Write-Host  > Docker-StopAllContainers
+Write-Host  > Docker-RemoveAllStoppedContainers
+Write-Host  > Docker-StartNginx
