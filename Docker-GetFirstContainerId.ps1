c186a3d2bf9102aa02ef9ed0332a241e94e6ec18
Paul Glavich <glav@theglavs.com>
2017-03-13 08:59:28 +1100

    Fixed minor script error

--- Docker-GetFirstContainerId.ps1
+++ Docker-GetFirstContainerId.ps1
@@ -1,5 +1,5 @@
 #set $id to 1st container id in list
 docker ps | foreach { if ($_ -inotmatch "CONTAINER ID") { $id = $_.Split(' ')[0] }} 
 
-"First container id is [$id]"
-"and is assigned to variable" $"id"
+Write-Host "First container id is [$id]"
+Write-Host "and is assigned to variable" $"id"
