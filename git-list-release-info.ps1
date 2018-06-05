c763430be212612a72367aa77e10ddcad1815b52
Paul Glavich <glav@theglavs.com>
2018-06-05 18:06:28 +1000

    Added some comments

--- git-list-release-info.ps1
+++ git-list-release-info.ps1
@@ -1,4 +1,5 @@
 #This script will get last tag from HEAD backwards in history
+# see https://discuss.bitrise.io/t/how-to-generate-release-notes-changelog-from-git-commits-since-last-git-tag/2941
 $lastTag = git describe --tags --abbrev=0
 "Last tag in history from HEAD is [$lastTag]"
 $releaseNotes = git log "$lastTag..HEAD" --pretty=format:"%h %s"
--- git-list-release-info.sh
+++ git-list-release-info.sh
@@ -1,1 +1,2 @@
+#See https://discuss.bitrise.io/t/how-to-generate-release-notes-changelog-from-git-commits-since-last-git-tag/2941
 git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%h %s"
