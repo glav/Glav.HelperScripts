b6a5a6a672f47738540b9054f5c4644245b3885e
Paul Glavich <glav@theglavs.com>
2021-11-11 13:39:30 +1100

    Updated Svc principal script

--- Azure-Create-Rbac-Principal.ps1
+++ Azure-Create-Rbac-Principal.ps1
@@ -1,5 +1,4 @@
 param(
-[Parameter(Mandatory = $true)]
 $subscriptionId,
 
 [Parameter(Mandatory = $true)]
@@ -7,4 +6,21 @@ $principalName
 
 
 )
+
+if ($null -eq $subscriptionId -or $subscriptionId -eq "")
+{
+    Write-Host "Subscription ID not provided, using current context if it exists.."
+    $currentAcct = (az account show) | ConvertFrom-Json
+    if ($null -eq $currentAcct -or $null -eq $currentAcct.id)
+    {
+      Write-Host "No subscription context available for use. Exiting."
+      return;
+    }
+    $subscriptionId = $currentAcct.id
+}
+
+Write-Host "Using subscription: $subscriptionId"
+
 az ad sp create-for-rbac --name $principalName --role contributor --scopes /subscriptions/$subscriptionId --sdk-auth
+
+Write-Host "Created Service Principal: '$principalName' with contributor role in Subscription: $subscriptionId ."
